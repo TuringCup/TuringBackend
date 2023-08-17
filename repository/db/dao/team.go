ae7c04a6d43db0690411a35b5f5d3b99d76fff15
vv12138vv <jgss987611676@outlook.com>
2023-08-17 10:45:19 +0800

    init teamDao

--- repository/db/dao/team.go
+++ repository/db/dao/team.go
@@ -1,1 +1,36 @@
 package dao
+
+import (
+	"context"
+	"github.com/TuringCup/TuringBackend/repository/db/model"
+	"gorm.io/gorm"
+)
+
+type TeamDao struct {
+	*gorm.DB
+}
+
+func NewTeamDao(ctx context.Context) *TeamDao {
+	return &TeamDao{NewDBClient(ctx)}
+}
+
+func TestNewTeamDao() *TeamDao {
+	return &TeamDao{TestDBClient()}
+}
+
+func (dao *TeamDao) CreateTeam(team *model.Team) error {
+	return dao.DB.Create(&team).Error
+}
+
+func (dao *TeamDao) ExistOrNotByTeamName(name string) (team *model.Team, exist bool, err error) {
+	var count int64
+	err = dao.DB.Model(&model.Race{}).Where("name=?", name).Count(&count).Error
+	if count == 0 {
+		return nil, false, err
+	}
+	err = dao.DB.Model(&model.Race{}).Where("name=?", name).First(&team).Error
+	if err != nil {
+		return team, true, err
+	}
+	return team, true, nil
+}
