895f17a59b18484fbbbcbf066ed29106f1ea5919
deltaLRD <ruidongli2002@gmail.com>
2023-10-13 22:03:56 +0800

    update

--- middleware/auth.go
+++ middleware/auth.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/TuringCup/TuringBackend/pkg/errors"
 	"github.com/TuringCup/TuringBackend/pkg/utils/jwt"
+	"github.com/TuringCup/TuringBackend/pkg/utils/logger"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,26 +16,30 @@ func AuthMiddleWare() gin.HandlerFunc {
 		token := ctx.Request.FormValue("token")
 		client_ip := ctx.ClientIP()
 		claim, err := jwt.ParseToken(token)
+		defer logger.Logger.Sync()
+		logger.Logger.Sugar().Info(token)
+		logger.Logger.Sugar().Error(claim)
 		if err != nil {
+			logger.Logger.Sugar().Error(err)
 			ctx.JSON(http.StatusForbidden, gin.H{
-				"ErrorCode": errors.Forbidden,
-				"ErrorMsg":  errors.GetMsg(errors.Forbidden),
+				"errorCode": errors.Forbidden,
+				"errorMsg":  errors.GetMsg(errors.Forbidden),
 			})
 			ctx.Abort()
 			return
 		}
 		if claim.IP != client_ip {
 			ctx.JSON(http.StatusForbidden, gin.H{
-				"ErrorCode": errors.Forbidden,
-				"ErrorMsg":  errors.GetMsg(errors.Forbidden) + " ip changed",
+				"errorCode": errors.Forbidden,
+				"errorMsg":  errors.GetMsg(errors.Forbidden) + " ip changed",
 			})
 			ctx.Abort()
 			return
 		}
 		if claim.ExpiresAt < time.Now().Unix() {
 			ctx.JSON(http.StatusForbidden, gin.H{
-				"ErrorCode": errors.TokenTimeout,
-				"ErrorMsg":  errors.GetMsg(errors.TokenTimeout),
+				"errorCode": errors.TokenTimeout,
+				"errorMsg":  errors.GetMsg(errors.TokenTimeout),
 			})
 			ctx.Abort()
 			return
